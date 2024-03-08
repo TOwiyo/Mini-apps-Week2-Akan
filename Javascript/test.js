bb55921b78fb5b086bd06a8e89391f45c6294a2a
Timothy Owiyo Opiyo <TOwiyo@safaricom.co.ke>
2024-03-08 12:34:05 +0300

    removed unused line

--- Javascript/test.js
+++ Javascript/test.js
@@ -38,7 +38,6 @@ function computeDayOfTheweek(){
 
     let dayOfTheWeek =parseInt(CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) %
     7;
-let updateddayoftheweek=dayOfTheWeek+1;
     return dayOfTheWeek;
     
 }
